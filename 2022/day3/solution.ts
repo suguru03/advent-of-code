4a5ffdbaee3e8f305c86268576dab932bc70bac5
Suguru Motegi <suguru.motegi@gmail.com>
2022-12-07 22:47:48 -0800

    Update day3

--- 2022/day3/solution.ts
+++ 2022/day3/solution.ts
@@ -3,33 +3,48 @@ import * as path from 'path';
 
 const file = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
 const rows = parseInput(file);
-console.log(getSum(rows));
-
-interface Row {
-  left: string;
-  right: string;
-}
+console.log(getSum1(rows));
+console.log(getSum2(rows));
 
 function parseInput(file: string) {
-  const list: Row[] = [];
+  const list: string[] = [];
   const rows = file.split(/\n/g);
   for (const row of rows) {
     if (row == '') {
       continue;
     }
-    const index = (row.length / 2) | 0;
-    list.push({ left: row.slice(0, index), right: row.slice(index) });
+    list.push(row);
   }
 
   return list;
 }
 
-function getSum(nums: Row[]) {
-  return nums.reduce((acc, row) => {
-    const leftSet = new Set(row.left);
-    const rightSet = new Set(row.right);
-    return Array.from(rightSet).reduce((acc, c) => acc + (leftSet.has(c) ? getScore(c) : 0), acc);
+function getSum1(rows: string[]) {
+  return rows.reduce((acc, row) => {
+    const index = (row.length / 2) | 0;
+    const left = new Set(row.slice(0, index));
+    const right = new Set(row.slice(index));
+    return acc + getScoreBySet(findCommon(left, right));
   }, 0);
+}
+
+function findCommon(left: Set<string>, right: Set<string>) {
+  return new Set(Array.from(left).filter((char) => right.has(char)));
+}
+
+function getSum2(rows: string[]) {
+  let sum = 0;
+  for (let i = 0; i < rows.length / 3; i++) {
+    const left = new Set(rows[i * 3]);
+    const mid = new Set(rows[i * 3 + 1]);
+    const right = new Set(rows[i * 3 + 2]);
+    sum += getScoreBySet(findCommon(findCommon(left, mid), right));
+  }
+  return sum;
+}
+
+function getScoreBySet(set: Set<string>) {
+  return Array.from(set).reduce((acc, c) => acc + getScore(c), 0);
 }
 
 function getScore(c: string) {
