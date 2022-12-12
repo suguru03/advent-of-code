ebae9e5c91767c2b6622903107c730f4631c160c
Suguru Motegi <suguru.motegi@gmail.com>
2022-12-12 13:13:49 -0800

    Update day8

--- 2022/day8/solution.ts
+++ 2022/day8/solution.ts
@@ -6,6 +6,8 @@ type Grid = number[][];
 const file = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8');
 const grid = parseInput(file);
 console.log(countTree(grid));
+console.log(findHighestScore(grid));
+
 function parseInput(file: string) {
   return file
     .trim()
@@ -38,5 +40,37 @@ function countTree(grid: Grid) {
     }
 
     find(x + dx, +y + dy, dx, dy, Math.max(cur, prev));
+  }
+}
+function findHighestScore(grid: Grid) {
+  const deltaList = [
+    { x: -1, y: 0 },
+    { x: 1, y: 0 },
+    { x: 0, y: -1 },
+    { x: 0, y: 1 },
+  ];
+  const yl = grid.length;
+  const xl = grid[yl - 1].length;
+  let max = 0;
+  for (let y = 1; y < yl - 1; y++) {
+    for (let x = 1; x < xl - 1; x++) {
+      const threshold = grid[y][x];
+      const sum = deltaList.reduce((acc, d) => acc * count(x + d.x, y + d.y, d.x, d.y, threshold), 1);
+      max = Math.max(max, sum);
+    }
+  }
+  return max;
+
+  function count(x: number, y: number, dx: number, dy: number, threshold: number) {
+    const cur = grid[y]?.[x] ?? null;
+    if (cur === null) {
+      return 0;
+    }
+
+    if (cur >= threshold) {
+      return 1;
+    }
+
+    return 1 + count(x + dx, +y + dy, dx, dy, threshold);
   }
 }
