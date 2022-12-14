183342996d494b490711da68803e9b908a49d857
Suguru Motegi <suguru.motegi@gmail.com>
2022-12-13 23:53:43 -0800

    Update day12

--- 2022/day12/solution.ts
+++ 2022/day12/solution.ts
@@ -5,6 +5,7 @@ import { Vector2 } from '../utils/vector';
 class Solution {
   solve1() {
     const [start, end, grid] = this.parse();
+    console.log(grid.map((row) => row.map((num) => num.toString().padStart(2, '0')).join(',')).join('\n'));
     const yl = grid.length;
     const xl = grid[yl - 1].length;
     const minGrid = Array.from(grid, (row) => Array.from(row, () => Infinity));
@@ -24,12 +25,11 @@ class Solution {
         return Infinity;
       }
 
+      minGrid[y][x] = count;
       if (vector.equal(end)) {
-        minGrid[y][x] = count;
         return count;
       }
 
-      minGrid[y][x] = count;
       return Math.min(
         findMin(vector.left, cur, count),
         findMin(vector.right, cur, count),
@@ -39,7 +39,44 @@ class Solution {
     }
   }
 
-  solve2() {}
+  solve2() {
+    const [_, start, grid] = this.parse();
+    const yl = grid.length;
+    const xl = grid[yl - 1].length;
+    const minGrid = Array.from(grid, (row) => Array.from(row, () => Infinity));
+    const seen = Array<number>(26).fill(0);
+    return findMin(start, grid[start.y][start.x], -1);
+
+    function findMin(vector: Vector2, prev: number, count: number) {
+      const { x, y } = vector;
+      if (x < 0 || x >= xl || y < 0 || y >= yl) {
+        return Infinity;
+      }
+      if (++count >= minGrid[y][x]) {
+        return Infinity;
+      }
+
+      const cur = grid[y][x];
+      if (!seen[cur] && prev - cur > 1) {
+        return Infinity;
+      }
+
+      minGrid[y][x] = count;
+      if (cur === 0) {
+        return count;
+      }
+
+      seen[cur]++;
+      const min = Math.min(
+        findMin(vector.left, cur, count),
+        findMin(vector.right, cur, count),
+        findMin(vector.up, cur, count),
+        findMin(vector.down, cur, count),
+      );
+      seen[cur]--;
+      return min;
+    }
+  }
 
   private parse(): [Vector2, Vector2, number[][]] {
     const base = 'a'.charCodeAt(0);
@@ -65,3 +102,4 @@ class Solution {
 }
 
 console.log(new Solution().solve1());
+console.log(new Solution().solve2());
