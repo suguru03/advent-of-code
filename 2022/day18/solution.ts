f8b51d1fd692a8a3b934ef3370c3fec85c38e5f3
Suguru Motegi <suguru.motegi@gmail.com>
2022-12-18 14:25:09 -0800

    Update day18

--- 2022/day18/solution.ts
+++ 2022/day18/solution.ts
@@ -10,21 +10,27 @@ const delta = [
   [0, 0, 1],
 ] as const;
 
+enum Item {
+  Unknown,
+  Cube,
+  Air,
+}
+
 class Solution {
   solve1() {
     const rows = this.parse();
-    const grid: number[][][] = [];
+    const cubes: number[][][] = [];
     for (const [x, y, z] of rows) {
-      grid[z] ??= [];
-      grid[z][y] ??= [];
-      grid[z][y][x] = 1;
+      cubes[z] ??= [];
+      cubes[z][y] ??= [];
+      cubes[z][y][x] = 1;
     }
 
     let count = 0;
     for (const row of rows) {
       for (const d of delta) {
         const [x, y, z] = row.map((n, i) => n + d[i]);
-        if (grid[z]?.[y]?.[x] === 1) {
+        if (cubes[z]?.[y]?.[x] === 1) {
           continue;
         }
         count++;
@@ -32,6 +38,68 @@ class Solution {
     }
 
     return count;
+  }
+
+  solve2() {
+    const rows = this.parse();
+    const cubes: number[][][] = [];
+    const minList = [Infinity, Infinity, Infinity];
+    const maxList = [-Infinity, -Infinity, -Infinity];
+    for (const row of rows) {
+      fill(...row, Item.Cube);
+      for (const [i, val] of row.entries()) {
+        minList[i] = Math.min(minList[i], val - 1);
+        maxList[i] = Math.max(maxList[i], val + 1);
+      }
+    }
+    let count = 0;
+    find(minList);
+    return count;
+
+    function find(row: number[]) {
+      const queue = [row];
+      while (queue.length) {
+        const row = queue.shift()!;
+        if (row.some((n, i) => n < minList[i] || n > maxList[i])) {
+          continue;
+        }
+        const [x, y, z] = row;
+        const item = cubes[z]?.[y]?.[x] ?? Item.Unknown;
+        if (item > Item.Unknown) {
+          if (item === Item.Cube) {
+            count++;
+          }
+          continue;
+        }
+
+        fill(x, y, z, Item.Air);
+        for (const d of delta) {
+          queue.push(row.map((n, i) => n + d[i]));
+        }
+      }
+
+      return false;
+    }
+
+    function fill(x: number, y: number, z: number, item: Item) {
+      cubes[z] ??= [];
+      cubes[z][y] ??= [];
+      cubes[z][y][x] = item;
+    }
+
+    function logCubes(cubes: number[][][]) {
+      for (const [z, c] of cubes.entries()) {
+        console.log(`#### ${z} ####`);
+        let str = '';
+        for (let y = minList[1]; y <= maxList[1]; y++) {
+          for (let x = minList[0]; x <= maxList[0]; x++) {
+            str += c?.[y]?.[x] ?? Item.Unknown;
+          }
+          str += '\n';
+        }
+        console.log(str.trim());
+      }
+    }
   }
 
   private parse(): [number, number, number][] {
@@ -42,3 +110,4 @@ class Solution {
 }
 
 console.log(new Solution().solve1());
+console.log(new Solution().solve2());
