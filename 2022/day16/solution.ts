b20374c318ae3f0468b0ec051d4dfd096417a4ce
Suguru Motegi <suguru.motegi@gmail.com>
2022-12-17 17:07:19 -0800

    Add day16

--- 2022/day16/solution.ts
+++ 2022/day16/solution.ts
@@ -1,13 +1,21 @@
 import * as path from 'path';
 import { File } from '../utils/file';
+import { start } from 'repl';
 
 type Valve = string;
 type Rate = number;
+type Distance = number;
 
 interface Data {
   valve: Valve;
   rate: Rate;
   valves: Valve[];
+}
+
+interface FlatData {
+  valve: Valve;
+  rate: Rate;
+  distanceMap: Map<Valve, Distance>;
 }
 
 interface Score {
@@ -17,92 +25,106 @@ interface Score {
 }
 
 class Solution {
+  private start = 'AA';
   solve1() {
     const map = this.parse();
-    let sum = 0;
-    let releasing = 0;
-    let current = 'AA';
-    let next = '';
-    const availableSet = new Set<Valve>(Array.from(map.keys()));
-    let min = 30;
-    let moveCount = 0;
-    while (min-- >= 0) {
-      sum += releasing;
-      if (moveCount > 0) {
-        moveCount--;
-        console.log('_(:3」∠)_________________', { current, next, sum, releasing, moveCount });
-        continue;
-      }
-
-      if (next !== '') {
-        availableSet.delete(next);
-        releasing += map.get(next)!.rate;
-        [current, next] = [next, ''];
-      }
-      const score = findHighestScore(current);
-      if (score === null) {
-        continue;
-      }
-      next = score.valve;
-      moveCount = score.depth;
-      console.log('_(:3」∠)_________________', { current, next, sum, releasing, moveCount });
+    const flatMap = this.flat(map);
+    let max = 0;
+    const timeLimit = 30;
+    const openedSet = new Set<Valve>();
+    const startData = flatMap.get(this.start)!;
+    // open A
+    if (startData.rate !== 0) {
+      dfs(this.start, 0, 0, 0);
+    }
+    // not open A
+    for (const [valve, distance] of startData.distanceMap) {
+      dfs(valve, 0, 0, distance);
     }
 
-    return sum;
+    return max;
 
-    function findHighestScore(current: Valve) {
-      const scores: Score[] = [];
-      const seen = new Set<Valve>();
-
-      const queue = [current];
-      let depth = 0;
-      while (queue.length) {
-        let target = '';
-        let targetDepth = 0;
-        let max = 0;
-        let count = queue.length;
-        let prevLoseScore = Math.max(0, ...scores.map((s) => (depth - s.depth) * s.max));
-        // console.log('_(:3」∠)_________________depth', depth);
-        while (--count >= 0) {
-          const cur = queue.shift()!;
-          if (seen.has(cur)) {
-            continue;
-          }
-          seen.add(cur);
-          const data = map.get(cur)!;
-          if (availableSet.has(cur)) {
-            const score = data.rate;
-            // console.log('_(:3」∠)_________________score', { cur, score, max, nextMax });
-            if (score > max) {
-              target = cur;
-              max = data.rate;
-              targetDepth = depth;
-            }
-          }
-          queue.push(...data.valves);
-        }
-        scores.push({ valve: target, depth, max });
-        depth++;
-      }
-      if (current === 'HH') {
-        console.log('_(:3」∠)_________________', availableSet);
-        console.log('_(:3」∠)_________________', scores);
+    function dfs(current: Valve, score: number, total: number, time: number) {
+      if (time === timeLimit) {
+        max = Math.max(max, total);
+        return;
       }
 
-      let highestScore: Score | null = null;
-      for (const score of scores) {
-        if (highestScore == null) {
-          highestScore = score;
+      if (openedSet.has(current)) {
+        max = Math.max(max, total + (timeLimit - time) * score);
+        return;
+      }
+
+      const data = flatMap.get(current)!;
+      openedSet.add(current);
+      // open
+      time++;
+      total += score;
+      score += data.rate;
+      for (const [next, distance] of data.distanceMap) {
+        if (time + distance > timeLimit) {
+          const diff = timeLimit - time;
+          dfs('', score, total + score * diff, timeLimit);
           continue;
         }
-        const prev = (score.depth - highestScore.depth + 1) * highestScore.max;
-        // console.log('_(:3」∠)_________________', { highestScore, score, prev });
-        if (score.max > prev) {
-          highestScore = score;
+        dfs(next, score, total + score * distance, time + distance);
+      }
+      openedSet.delete(current);
+    }
+  }
+
+  private flat(map: Map<Valve, Data>): Map<Valve, FlatData> {
+    const flatMap = new Map<Valve, FlatData>();
+    resolve();
+
+    const ignoreMap = Array.from(map.values())
+      .filter((data) => data.rate === 0)
+      .map((data) => data.valve);
+    for (const data of flatMap.values()) {
+      for (const target of ignoreMap) {
+        data.distanceMap.delete(target);
+      }
+    }
+
+    return flatMap;
+
+    function resolve() {
+      for (const [valve, data] of map) {
+        flatMap.set(valve, {
+          valve,
+          rate: data.rate,
+          distanceMap: new Map<Valve, Distance>(data.valves.map((valve) => [valve, 1])),
+        });
+      }
+      const size = map.size - 1;
+      const queue = Array.from(map.keys());
+      while (queue.length) {
+        const current = queue.shift()!;
+        const data = map.get(current)!;
+        const flatData = flatMap.get(current)!;
+        for (const target of data.valves) {
+          const targetFlatData = flatMap.get(target)!;
+          for (const [child, distance] of targetFlatData.distanceMap) {
+            if (current === child) {
+              continue;
+            }
+            const min = Math.min(distance + 1, flatData.distanceMap.get(child) ?? Infinity);
+            flatData.distanceMap.set(child, min);
+          }
+
+          for (const [child, distance] of flatData.distanceMap) {
+            if (target === child) {
+              continue;
+            }
+            const min = Math.min(distance + 1, targetFlatData.distanceMap.get(child) ?? Infinity);
+            targetFlatData.distanceMap.set(child, min);
+          }
+          if (flatData.distanceMap.size === size) {
+            continue;
+          }
+          queue.push(current);
         }
       }
-
-      return highestScore;
     }
   }
 
