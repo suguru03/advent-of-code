946a1a3d9fa386af13bfbc11584272001ccd1582
Suguru Motegi <suguru.motegi@gmail.com>
2022-12-17 22:02:50 -0800

    Update day16

--- 2022/day16/solution.ts
+++ 2022/day16/solution.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 import { File } from '../utils/file';
-import { start } from 'repl';
 
 type Valve = string;
 type Rate = number;
@@ -18,22 +17,21 @@ interface FlatData {
   distanceMap: Map<Valve, Distance>;
 }
 
-interface Score {
+interface Status {
   valve: Valve;
-  depth: number;
-  max: number;
+  delay: number;
+  history?: string;
 }
 
 class Solution {
   private start = 'AA';
-  private dummyStart = '00';
   solve1() {
     const map = this.parse();
     const flatMap = this.flat(map);
     let max = 0;
     const timeLimit = 30;
     const openedSet = new Set<Valve>();
-    open(this.dummyStart, 0, 0, -1);
+    open(this.start, 0, 0, -1);
     return max;
 
     function open(current: Valve, score: number, total: number, time: number) {
@@ -65,12 +63,123 @@ class Solution {
     }
   }
 
+  solve2() {
+    const map = this.parse();
+    const flatMap = this.flat(map);
+    let max = 0;
+    const timeLimit = 26;
+    const openedSet = new Set<Valve>([this.start]);
+    const status = { valve: this.start, delay: 0, history: this.start };
+    open(status, { ...status }, 0, 0, -1);
+    return max;
+
+    function open(current1: Status, current2: Status, score: number, total: number, time: number) {
+      current1 = { ...current1 };
+      current2 = { ...current2 };
+      if (time === timeLimit) {
+        max = Math.max(max, total);
+        return;
+      }
+
+      if (current1.delay < 0 && current2.delay < 0) {
+        max = Math.max(max, total + (timeLimit - time) * score);
+        return;
+      }
+
+      time++;
+      total += score;
+
+      let data1 = flatMap.get(current1.valve)!;
+      let data2 = flatMap.get(current2.valve)!;
+      let update1 = false;
+      let update2 = false;
+      if (--current1.delay === -1) {
+        score += data1.rate;
+        update1 = true;
+      }
+      if (--current2.delay === -1) {
+        score += data2.rate;
+        update2 = true;
+      }
+
+      if (!update1 && !update2) {
+        open(current1, current2, score, total, time);
+        return;
+      }
+      if (!update1 && update2) {
+        [data1, data2] = [data2, data1];
+        [current1, current2] = [current2, current1];
+        [update1, update2] = [update2, update1];
+      }
+
+      if (update1 && !update2) {
+        let opened = false;
+        for (const [next, distance] of data1.distanceMap) {
+          if (openedSet.has(next)) {
+            continue;
+          }
+          opened = true;
+          openedSet.add(next);
+          open({ valve: next, delay: distance }, current2, score, total, time);
+          openedSet.delete(next);
+        }
+        if (!opened) {
+          open(current1, current2, score, total, time);
+        }
+        return;
+      }
+
+      let opened1 = false;
+      for (const [n1, d1] of data1.distanceMap) {
+        if (openedSet.has(n1)) {
+          continue;
+        }
+        opened1 = true;
+        openedSet.add(n1);
+        const next1 = { valve: n1, delay: d1 };
+
+        let opened2 = false;
+        for (const [n2, d2] of data2.distanceMap) {
+          if (openedSet.has(n2)) {
+            continue;
+          }
+          opened2 = true;
+          openedSet.add(n2);
+          open(next1, { valve: n2, delay: d2 }, score, total, time);
+          openedSet.delete(n2);
+        }
+        if (!opened2) {
+          open(next1, current2, score, total, time);
+        }
+        openedSet.delete(n1);
+      }
+      if (opened1) {
+        return;
+      }
+
+      let opened2 = false;
+      for (const [n2, d2] of data2.distanceMap) {
+        if (openedSet.has(n2)) {
+          continue;
+        }
+        opened2 = true;
+        openedSet.add(n2);
+        open(current1, { valve: n2, delay: d2 }, score, total, time);
+        openedSet.delete(n2);
+      }
+      if (opened2) {
+        return;
+      }
+      open(current1, current2, score, total, time);
+    }
+  }
+
   private flat(map: Map<Valve, Data>): Map<Valve, FlatData> {
     const flatMap = new Map<Valve, FlatData>();
     resolve();
 
     const ignoreMap = Array.from(map.values())
-      .filter((data) => data.rate === 0)
+      .filter((data, i) => data.rate === 0)
       .map((data) => data.valve);
     for (const data of flatMap.values()) {
       for (const target of ignoreMap) {
@@ -78,22 +187,16 @@ class Solution {
       }
     }
 
-    const dummyStep: FlatData = {
-      valve: this.dummyStart,
-      rate: 0,
-      distanceMap: new Map([[this.start, -1]]),
-    };
-    flatMap.set(dummyStep.valve, dummyStep);
-
     return flatMap;
 
     function resolve() {
       for (const [valve, data] of map) {
-        flatMap.set(valve, {
-          valve,
-          rate: data.rate,
-          distanceMap: new Map<Valve, Distance>(data.valves.map((valve) => [valve, 1])),
-        });
+        distanceMap: new Map<Valve, Distance>(data.valves.map((valve) => [valve, 1])),
+          flatMap.set(valve, {
+            valve,
+            rate: data.rate,
+            distanceMap: new Map<Valve, Distance>(data.valves.map((valve) => [valve, 1])),
+          });
       }
       const size = map.size - 1;
       const queue = Array.from(map.keys());
@@ -150,3 +253,4 @@ class Solution {
 }
 
 console.log(new Solution().solve1());
+console.log(new Solution().solve2());
