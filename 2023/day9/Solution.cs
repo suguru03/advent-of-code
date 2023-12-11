6eade7acb97bf447ce39ccecf2ec87e0e0585b7f
Suguru Motegi <suguru.motegi@gmail.com>
2023-12-10 18:59:12 -0800

    Add 2023 day 9 part 1 [cs]

---
+++ 2023/day9/Solution.cs
@@ -0,0 +1,57 @@
+using advent_of_code.Common;
+
+namespace advent_of_code._2023.day9;
+
+public class Solution : SolutionBase
+{
+    public override object Run(int part, bool useExample)
+    {
+        var input = Parse(useExample);
+        return part switch
+        {
+            1 => Solve1(input),
+            _ => ProblemNotSolvedString
+        };
+    }
+
+    private static int Solve1(IEnumerable<IEnumerable<int>> data)
+    {
+        var sum = 0;
+        foreach (var row in data)
+        {
+            var list = new List<List<int>> { row.ToList() };
+            bool hasValue;
+            do
+            {
+                hasValue = false;
+                var prev = list.Last();
+                var size = prev.Count - 1;
+                var next = new List<int>(size);
+                list.Add(next);
+                for (var i = 0; i < size; i++)
+                {
+                    var diff = prev[i + 1] - prev[i];
+                    hasValue |= diff != 0;
+                    next.Add(diff);
+                }
+            } while (hasValue);
+
+            for (var i = list.Count - 1; i >= 1; i--)
+            {
+                var prev = list[i - 1];
+                var current = list[i];
+                prev.Add(prev.Last() + current.Last());
+            }
+
+            sum += list.First().Last();
+        }
+
+        return sum;
+    }
+
+    private IEnumerable<IEnumerable<int>> Parse(bool useExample)
+    {
+        return ParseLines<IEnumerable<int>>(useExample ? "example.txt" : "input.txt",
+            text => text.Split(" ").Select(int.Parse));
+    }
+}
