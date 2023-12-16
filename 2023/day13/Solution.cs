9c1d09f719e93dbe24bf11e2229efa4026413b58
Suguru Motegi <suguru.motegi@gmail.com>
2023-12-15 18:19:13 -0800

    Add 2023 day 13 part 2 [cs]

--- 2023/day13/Solution.cs
+++ 2023/day13/Solution.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using advent_of_code.Common;
 
 namespace advent_of_code._2023.day13;
@@ -11,19 +10,29 @@ public class Solution : SolutionBase
         return part switch
         {
             1 => Solve1(input),
+            2 => Solve2(input),
             _ => ProblemNotSolvedString
         };
     }
 
     private static int Solve1(IEnumerable<Data> list)
     {
+        return Solve(list, FindPerfectMirrorIndex);
+    }
+
+    private static int Solve2(IEnumerable<Data> list)
+    {
+        return Solve(list, FindPerfectMirrorWithSmudgeIndex);
+    }
+
+    private static int Solve(IEnumerable<Data> list, Func<IList<int>, int> finder)
+    {
         return list.Select(data =>
         {
             var grid = data.Grid;
-            var str = string.Join('\n', grid.Select(r => new string(r.ToArray())));
             var rows = grid.Select(row => row.Aggregate(0, (cur, c) => (cur << 1) + Convert.ToInt32(c == '#')))
                 .ToList();
-            var index = FindPerfectMirrorIndex(rows);
+            var index = finder(rows);
             if (index != -1)
             {
                 return (index + 1) * 100;
@@ -31,7 +40,7 @@ public class Solution : SolutionBase
 
             var columns = grid.First().Select((_, x) =>
                 grid.Aggregate(0, (cur, row) => (cur << 1) + Convert.ToInt32(row[x] == '#'))).ToList();
-            return FindPerfectMirrorIndex(columns) + 1;
+            return finder(columns) + 1;
         }).Sum();
     }
 
@@ -53,6 +62,46 @@ public class Solution : SolutionBase
         }
 
         return -1;
+    }
+
+    private static int FindPerfectMirrorWithSmudgeIndex(IList<int> list)
+    {
+        for (var i = 1; i < list.Count; i++)
+        {
+            var left = i - 1;
+            var right = i;
+            var smudge = false;
+            while (list[left] == list[right] || !smudge && (smudge = HasSmudge(list[left], list[right])))
+            {
+                if (--left >= 0 && ++right < list.Count)
+                {
+                    continue;
+                }
+
+                if (!smudge)
+                {
+                    break;
+                }
+
+                return i - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private static bool HasSmudge(int a, int b)
+    {
+        return BitCount(a ^ b) == 1;
+    }
+
+    private static int BitCount(int x)
+    {
+        x = ((x >> 1) & 0x55555555) + (x & 0x55555555);
+        x = ((x >> 2) & 0x33333333) + (x & 0x33333333);
+        x = ((x >> 4) & 0x0f0f0f0f) + (x & 0x0f0f0f0f);
+        x = ((x >> 8) & 0x00ff00ff) + (x & 0x00ff00ff);
+        return ((x >> 16) & 0x0000ffff) + (x & 0x0000ffff);
     }
 
     private IEnumerable<Data> Parse(bool useExample)
