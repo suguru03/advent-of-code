d32decc3e3439a09750ab37aa9da1f95d4bc4299
Suguru Motegi <suguru.motegi@gmail.com>
2023-12-26 18:09:54 -0800

    Add 2023 day 19 part 2 [cs]

--- 2023/day19/Solution.cs
+++ 2023/day19/Solution.cs
@@ -1,16 +1,23 @@
 using System.Text.RegularExpressions;
 using advent_of_code.Common;
+using advent_of_code.Common.utils;
 
 namespace advent_of_code._2023.day19;
 
 public partial class Solution : SolutionBase
 {
+    private const string Start = "in";
+    private const string Accepted = "A";
+    private const string Rejected = "R";
+    private static readonly Range<int> RatingRange = new(1, 4000);
+
     public override object Run(int part, bool useExample)
     {
         var input = Parse(useExample);
         return part switch
         {
             1 => Solve1(input),
+            2 => Solve2(input),
             _ => ProblemNotSolvedString
         };
     }
@@ -25,17 +32,73 @@ public partial class Solution : SolutionBase
     private static bool IsAccepted(IReadOnlyDictionary<string, Workflow> workflowMap,
         IReadOnlyDictionary<char, int> ratingMap)
     {
-        const string start = "in";
-        const string accepted = "A";
-        const string rejected = "R";
-        var current = start;
-        while (current != accepted && current != rejected)
+        var current = Start;
+        while (current != Accepted && current != Rejected)
         {
             current = workflowMap[current].Expressions.Find(expression => expression.If(ratingMap))!.Consequent;
         }
 
-        return current == accepted;
+        return current == Accepted;
     }
+
+    private static long Solve2(Data data)
+    {
+        var workflowMap = new Dictionary<Expression, Workflow>();
+        var expressionsMap = new Dictionary<string, List<Expression>>();
+        foreach (var workflow in data.WorkflowMap.Values)
+        foreach (var expression in workflow.Expressions)
+        {
+            workflowMap.Add(expression, workflow);
+            if (!expressionsMap.TryGetValue(expression.Consequent, out var list))
+            {
+                list = new List<Expression>();
+                expressionsMap.Add(expression.Consequent, list);
+            }
+
+            list.Add(expression);
+        }
+
+        var chars = data.RatingMaps.First().Keys;
+        return expressionsMap[Accepted].Sum(expression =>
+            SumCombinations(expression, workflowMap, expressionsMap, chars.ToDictionary(c => c, _ => RatingRange)));
+    }
+
+    private static long SumCombinations(Expression current, IReadOnlyDictionary<Expression, Workflow> workflowMap,
+        IReadOnlyDictionary<string, List<Expression>> expressionsMap,
+        Dictionary<char, Range<int>> rangeMap)
+    {
+        UpdateRange(current.Condition, rangeMap, false);
+        var (workflowId, expressions) = workflowMap[current];
+        foreach (var prevExpression in expressions)
+        {
+            if (prevExpression == current)
+            {
+                break;
+            }
+
+            UpdateRange(prevExpression.Condition, rangeMap, true);
+        }
+
+        return workflowId == Start
+            ? rangeMap.Values.Aggregate(1L, (acc, range) => acc * (range.Max - range.Min + 1))
+            : expressionsMap[workflowId].Sum(next => SumCombinations(next, workflowMap, expressionsMap, rangeMap));
+    }
+
+    private static void UpdateRange(Condition? condition, IDictionary<char, Range<int>> rangeMap, bool reverse)
+    {
+        if (condition == null)
+        {
+            return;
+        }
+
+        rangeMap[condition.Category] = GetAndRange(rangeMap[condition.Category], condition.GetRange(reverse));
+    }
+
+    private static Range<int> GetAndRange(Range<int> lhs, Range<int> rhs)
+    {
+        return new Range<int>(Math.Max(lhs.Min, rhs.Min), Math.Min(lhs.Max, rhs.Max));
+    }
+
 
     private Data Parse(bool useExample)
     {
@@ -64,12 +127,30 @@ public partial class Solution : SolutionBase
 
     private record Workflow(string Id, List<Expression> Expressions);
 
-    private record Condition(char Category, char Operator, int Value);
+    private record Condition(char Category, char Operator, int Value)
+    {
+        public bool Check(int value)
+        {
+            return Operator == '>' ? value > Value : value < Value;
+        }
+
+        public Range<int> GetRange(bool reverse = false)
+        {
+            var greater = Operator == '>';
+            return reverse
+                ? greater
+                    ? RatingRange with { Max = Value }
+                    : RatingRange with { Min = Value }
+                : greater
+                    ? RatingRange with { Min = Value + 1 }
+                    : RatingRange with { Max = Value - 1 };
+        }
+    }
 
     private class Expression
     {
         public string Consequent { get; }
-        private Condition? Condition { get; }
+        public Condition? Condition { get; }
 
         public Expression(string text)
         {
@@ -86,18 +167,7 @@ public partial class Solution : SolutionBase
 
         public bool If(IReadOnlyDictionary<char, int> ratingMap)
         {
-            if (Condition == null)
-            {
-                return true;
-            }
-
-            var value = ratingMap[Condition.Category];
-            return Condition?.Operator switch
-            {
-                '<' => value < Condition.Value,
-                '>' => value > Condition.Value,
-                _ => throw new ArgumentOutOfRangeException()
-            };
+            return Condition?.Check(ratingMap[Condition.Category]) ?? true;
         }
     }
 
