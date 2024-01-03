a24c39011607d93b9158f1481021362481d549ce
Suguru Motegi <suguru.motegi@gmail.com>
2024-01-02 18:33:01 -0800

    Add 2023 day 22 part 1 [cs]

---
+++ 2023/day22/Solution.cs
@@ -0,0 +1,124 @@
+using System.Text.RegularExpressions;
+using advent_of_code.Common;
+
+namespace advent_of_code._2023.day22;
+
+public partial class Solution : SolutionBase
+{
+    public override object Run(int part, bool useExample)
+    {
+        var input = Parse(useExample);
+        return part switch
+        {
+            1 => Solve1(input),
+            _ => ProblemNotSolvedString
+        };
+    }
+
+    private static int Solve1(IEnumerable<Cube> snapshot)
+    {
+        return Settle(snapshot).Count(cube => cube.Disintegrable);
+    }
+
+    private static IEnumerable<Cube> Settle(IEnumerable<Cube> snapshot)
+    {
+        var cubes = snapshot.ToList();
+        cubes.Add(new Cube(new Coordinate(0, 0, 0), new Coordinate(int.MaxValue, int.MaxValue, 0)));
+        cubes.Sort((c1, c2) => c1.From.Z.CompareTo(c2.From.Z));
+        for (var i = 1; i < cubes.Count; i++)
+        {
+            var target = cubes[i];
+            var candidates = new List<Cube>();
+            for (var j = i - 1; j >= 0; j--)
+            {
+                var supporter = cubes[j];
+                if (target.From.Z <= supporter.To.Z ||
+                    target.From.X > supporter.To.X || target.To.X < supporter.From.X ||
+                    target.From.Y > supporter.To.Y || target.To.Y < supporter.From.Y)
+                {
+                    continue;
+                }
+
+
+                if (candidates.Count == 0)
+                {
+                    candidates.Add(supporter);
+                    continue;
+                }
+
+                var first = candidates.First();
+                if (supporter.To.Z < first.To.Z)
+                {
+                    continue;
+                }
+
+                if (supporter.To.Z > candidates.First().To.Z)
+                {
+                    candidates.Clear();
+                }
+
+                candidates.Add(supporter);
+            }
+
+            if (candidates.Count == 0)
+            {
+                continue;
+            }
+
+            var shift = target.From.Z - candidates.First().To.Z - 1;
+            target.From.Z -= shift;
+            target.To.Z -= shift;
+            foreach (var supporter in candidates)
+            {
+                supporter.Support(target);
+            }
+        }
+
+        return cubes;
+    }
+
+    private IEnumerable<Cube> Parse(bool useExample)
+    {
+        var cubeRegex = CubeRegex();
+        return ParseLines(useExample ? "example.txt" : "input.txt", text =>
+        {
+            var groups = cubeRegex.Match(text).Groups;
+            return new Cube(ParseCoordinate(groups["From"].Value), ParseCoordinate(groups["To"].Value));
+        }).ToList();
+    }
+
+    private static Coordinate ParseCoordinate(string input)
+    {
+        var nums = input.Split(',').Select(str => int.Parse(str.Trim())).ToList();
+        return new Coordinate(nums[0], nums[1], nums[2]);
+    }
+
+    private class Coordinate(int x, int y, int z)
+    {
+        public int X => x;
+        public int Y => y;
+        public int Z { get; set; } = z;
+    }
+
+    private record Cube(Coordinate From, Coordinate To)
+    {
+        private readonly HashSet<Cube> _supporting = [];
+        private readonly HashSet<Cube> _supported = [];
+        public bool Disintegrable => _supporting.All(cube => cube.IsSupported(this));
+
+        public void Support(Cube cube)
+        {
+            _supporting.Add(cube);
+            cube._supported.Add(this);
+        }
+
+
+        private bool IsSupported(Cube exclude)
+        {
+            return From.Z <= exclude.To.Z || _supported.Any(cube => cube != exclude && cube.IsSupported(exclude));
+        }
+    }
+
+    [GeneratedRegex(@"(?<From>-?.+)\~(?<To>-?.+)")]
+    private static partial Regex CubeRegex();
+}
