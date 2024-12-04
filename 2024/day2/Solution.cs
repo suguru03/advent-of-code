6b82758fc69338051e8588a9eccce0cce297b3ee
Suguru Motegi <suguru.motegi@gmail.com>
2024-12-03 21:08:18 -0800

    Add 2024 day 2 part 2 [cs]

--- 2024/day2/Solution.cs
+++ 2024/day2/Solution.cs
@@ -1,47 +1,59 @@
-using System.Text.RegularExpressions;
 using advent_of_code.Common;
 
 namespace advent_of_code._2024.day2;
 
-public partial class Solution : SolutionBase
+public class Solution : SolutionBase
 {
     public override object Run(int part, bool useExample)
     {
-        var input = ParseLines(!useExample ? "input.txt" : part == 1 ? "example1.txt" : "example2.txt",
+        var input = ParseLines(!useExample ? "input.txt" : "example.txt",
             row => row.Split(" ").Select(int.Parse).ToArray());
         return part switch
         {
             1 => Solve1(input),
+            2 => Solve2(input),
             _ => ProblemNotSolvedString
         };
     }
 
     private static int Solve1(IEnumerable<int[]> parseLines)
     {
-        return parseLines.Count(row =>
+        return parseLines.Count(IsSafe);
+    }
+
+    private static int Solve2(IEnumerable<int[]> parseLines)
+    {
+        return parseLines.Count(rows => IsSafe(rows) || rows.Where((_, i) => IsSafe(RemoveAt(rows, i))).Any());
+    }
+
+    private static bool IsSafe(int[] rows)
+    {
+        var sign = Math.Sign(rows[1] - rows[0]);
+        if (sign == 0)
         {
-            var sign = Math.Sign(row[1] - row[0]);
-            if (sign == 0)
+            return false;
+        }
+
+        for (var left = 0; left < rows.Length - 1; left++)
+        {
+            var right = left + 1;
+            if (Math.Sign(rows[right] - rows[left]) != sign)
             {
                 return false;
             }
 
-            for (var left = 0; left < row.Length - 1; left++)
+            var diff = Math.Abs(rows[right] - rows[left]);
+            if (diff is < 1 or > 3)
             {
-                var right = left + 1;
-                if (Math.Sign(row[right] - row[left]) != sign)
-                {
-                    return false;
-                }
-
-                var diff = Math.Abs(row[right] - row[left]);
-                if (diff is < 1 or > 3)
-                {
-                    return false;
-                }
+                return false;
             }
+        }
 
-            return true;
-        });
+        return true;
+    }
+
+    private static int[] RemoveAt(int[] rows, int index)
+    {
+        return rows.Take(index).Concat(rows.Skip(index + 1)).ToArray();
     }
 }
