25a333a586561f5c08340b298051ac77b1e896f0
Suguru Motegi <suguru.motegi@gmail.com>
2024-12-03 21:52:46 -0800

    Add 2024 day 3 part 2 [cs]

--- 2024/day3/Solution.cs
+++ 2024/day3/Solution.cs
@@ -7,21 +7,61 @@ public partial class Solution : SolutionBase
 {
     public override object Run(int part, bool useExample)
     {
-        var inputRegex = InputRegex();
-        var input = ParseText(!useExample ? "input.txt" : "example.txt",
-            row => inputRegex.Matches(row).Select(m => (int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value))));
         return part switch
         {
-            1 => Solve1(input),
+            1 => Solve1(useExample),
+            2 => Solve2(useExample),
             _ => ProblemNotSolvedString
         };
     }
 
-    private static int Solve1(IEnumerable<(int left, int right)> rows)
+    private int Solve1(bool useExample)
     {
-        return rows.Sum(tuple => tuple.left * tuple.right);
+        var inputRegex = Input1Regex();
+        return ParseText(!useExample ? "input.txt" : "example1.txt", row => inputRegex.Matches(row))
+            .Select(m => (left: int.Parse(m.Groups[1].Value), right: int.Parse(m.Groups[2].Value)))
+            .Sum(tuple => tuple.left * tuple.right);
     }
 
     [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
-    private static partial Regex InputRegex();
+    private static partial Regex Input1Regex();
+
+    private int Solve2(bool useExample)
+    {
+        var inputRegex = Input2Regex();
+        var matches = ParseText(!useExample ? "input.txt" : "example2.txt", row => inputRegex.Matches(row));
+        var sum = 0;
+        var enabled = true;
+        foreach (Match match in matches)
+        {
+            switch (match.Value)
+            {
+                case "do()":
+                {
+                    enabled = true;
+                    break;
+                }
+                case "don't()":
+                {
+                    enabled = false;
+                    break;
+                }
+                default:
+                {
+                    if (!enabled)
+                    {
+                        break;
+                    }
+
+                    sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+                    break;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)|do(n\'t)?\(\)")]
+    private static partial Regex Input2Regex();
 }
